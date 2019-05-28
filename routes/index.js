d0d66479247d1ef07f025a596ded811a7f06adf9
Ellen Lee <leechinghui.tw@gmail.com>
2019-05-28 14:42:50 +0800

    Add admin restaurant index page

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ module.exports = app => {
   // 如果使用者訪問首頁，就導向 /restaurants 的頁面
   app.get('/', (req, res) => res.redirect('restaurants'))
   app.get('/restaurants', restController.getRestaurants)
+
+  // 連到 /admin 頁面就轉到 /admin/restaurants
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', adminController.getRestaurants)
 }
