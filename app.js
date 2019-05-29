93b732dfda82ead36b8b6b4f8de3f012a7e76ad4
Ellen Lee <leechinghui.tw@gmail.com>
2019-05-29 19:35:27 +0800

    Install express-session connect-flash

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+
 const app = express()
 const port = 3000
 
 // 設定 view engine 使用 handlebars
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 
 app.listen(port, () => {
   db.sequelize.sync() // 跟資料庫同步
