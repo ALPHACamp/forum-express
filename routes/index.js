b325c9a2ad02b2567347bf6295598a1caf4911ad
Ellen Lee <leechinghui.tw@gmail.com>
2019-05-29 15:29:49 +0800

    Add userController and signup page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 
 module.exports = app => {
   // 如果使用者訪問首頁，就導向 /restaurants 的頁面
@@ -9,4 +10,7 @@ module.exports = app => {
   // 連到 /admin 頁面就轉到 /admin/restaurants
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
