6fceb08fe56228416820998e1beb13709acbb2e7
Ellen Lee <leechinghui.tw@gmail.com>
2019-05-30 03:46:10 +0800

    Add image to restaurants

--- models/restaurant.js
+++ models/restaurant.js
@@ -5,10 +5,11 @@ module.exports = (sequelize, DataTypes) => {
     tel: DataTypes.STRING,
     address: DataTypes.STRING,
     opening_hours: DataTypes.STRING,
-    description: DataTypes.TEXT
+    description: DataTypes.TEXT,
+    image: DataTypes.STRING
   }, {})
   Restaurant.associate = function (models) {
     // associations can be defined here
   }
   return Restaurant
-};
+}
