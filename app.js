1b15ef7dc282e373b8314aad0e0b21ce242aba4f
Ellen Lee <leechinghui.tw@gmail.com>
2019-05-29 22:23:48 +0800

    Install passport and passport-local

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./models')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
@@ -14,6 +15,8 @@ app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 // 把 req.flash 放到 res.locals 裡面
@@ -28,4 +31,4 @@ app.listen(port, () => {
 })
 
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
-require('./routes')(app)
+require('./routes')(app, passport)
