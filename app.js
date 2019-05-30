710adf11a36f7f9190b37b317cd909c68f1d715a
Ellen Lee <leechinghui.tw@gmail.com>
2019-05-30 08:18:19 +0800

    Listen to process.env.PORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // 設定 view engine 使用 handlebars
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
