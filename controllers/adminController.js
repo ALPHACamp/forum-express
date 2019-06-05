5781fa843111b63d17233d7d7163d20978b7baf6
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-05 12:04:14 +0800

    Show category name on admin/restaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,10 +4,12 @@ const fs = require('fs')
 const db = require('../models')
 const Restaurant = db.Restaurant
 const User = db.User
+const Category = db.Category
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll().then(restaurants => {
+    return Restaurant.findAll({ include: [Category] }).then(restaurants => {
+      console.log(restaurants)
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
   },
