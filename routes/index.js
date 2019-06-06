565647a9c9a52f1edf936fdbc9f81678bd62ab69
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-06 15:26:56 +0800

    Add admin categories edit and put feature

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,8 @@ module.exports = (app, passport) => {
 
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
+  app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
+  app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
