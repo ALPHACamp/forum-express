0a04b55851d7eb5700c2fd3a926ae8b9326b26e4
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-08 19:55:27 +0800

    Add restaurants show page

--- controllers/restController.js
+++ controllers/restController.js
@@ -13,6 +13,15 @@ const restController = {
         restaurants: data
       })
     })
+  },
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    }).then(restaurant => {
+      return res.render('restaurant', {
+        restaurant: restaurant
+      })
+    })
   }
 }
 
