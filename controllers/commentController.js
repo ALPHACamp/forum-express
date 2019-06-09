3f6107a1b0e7d6b91bbf64259427d120db69e577
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-09 18:42:52 +0800

    Add delete comment feature for admin

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -11,6 +11,15 @@ let commentController = {
       .then((restaurant) => {
         res.redirect(`/restaurants/${req.body.restaurantId}`)
       })
+  },
+  deleteComment: (req, res) => {
+    return Comment.findByPk(req.params.id)
+      .then((comment) => {
+        comment.destroy()
+          .then((comment) => {
+            res.redirect(`/restaurants/${comment.RestaurantId}`)
+          })
+      })
   }
 }
 
