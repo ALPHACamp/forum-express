9b26f2d75b018f49ccd397593c4956653de6fd84
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-10 18:55:06 +0800

    Add user profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,6 +45,11 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res) => {
+    return User.findByPk(req.params.id).then(user => {
+      return res.render('users/profile', { profile: user })
+    })
   }
 }
 
