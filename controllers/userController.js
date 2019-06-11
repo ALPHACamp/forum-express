ced076256b0f7d230e42ef524f639cc359115ca5
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-11 14:31:31 +0800

    Add commented restaurants on user profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
+const Restaurant = db.Restaurant
+const Comment = db.Comment
 
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
@@ -50,7 +52,11 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res) => {
-    return User.findByPk(req.params.id).then(user => {
+    return User.findByPk(req.params.id, {
+      include: [
+        { model: Comment, include: Restaurant }
+      ]
+    }).then(user => {
       return res.render('users/profile', { profile: user })
     })
   },
