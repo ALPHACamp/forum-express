58f7d995717fb323cb4845f257e71f50b2c8872d
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-14 15:51:06 +0800

    Add feeds page

--- controllers/restController.js
+++ controllers/restController.js
@@ -55,6 +55,24 @@ const restController = {
         restaurant: restaurant
       })
     })
+  },
+  getFeeds: (req, res) => {
+    return Restaurant.findAll({
+      limit: 10,
+      order: [['createdAt', 'DESC']],
+      include: [Category]
+    }).then(restaurants => {
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant]
+      }).then(comments => {
+        return res.render('feeds', {
+          restaurants: restaurants,
+          comments: comments
+        })
+      })
+    })
   }
 }
 
