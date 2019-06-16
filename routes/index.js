9a7f8939a36deeff6f88dddd5451a85b4f2b32b7
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-16 15:42:33 +0800

    Add addFavorite and removeFavorite feature

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,8 @@ module.exports = (app, passport) => {
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
   app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+  app.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+  app.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 
   // 連到 /admin 頁面就轉到 /admin/restaurants
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
