2e5346c091f1736bfe3b3f8f1943bd0067ba06d4
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-16 18:56:07 +0800

    Create fellowship model and add followers and followings to users

--- config/passport.js
+++ config/passport.js
@@ -31,7 +31,9 @@ passport.deserializeUser((id, cb) => {
   User.findByPk(id, {
     include: [
       { model: db.Restaurant, as: 'FavoritedRestaurants' },
-      { model: db.Restaurant, as: 'LikedRestaurants' }
+      { model: db.Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
     ]
   }).then(user => {
     return cb(null, user)
