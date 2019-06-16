3e6977b918c5a6f7e9e4d56d2ceb2b14112c7dbf
Ellen Lee <leechinghui.tw@gmail.com>
2019-06-16 19:42:21 +0800

    Add topUser page

--- controllers/userController.js
+++ controllers/userController.js
@@ -141,6 +141,26 @@ const userController = {
           return res.redirect('back')
         })
     })
+  },
+  getTopUser: (req, res) => {
+    // 撈出所有 User 與 followers 資料
+    return User.findAll({
+      include: [
+        { model: User, as: 'Followers' }
+      ]
+    }).then(users => {
+      // 整理 users 資料
+      users = users.map(user => ({
+        ...user.dataValues,
+        // 計算追蹤者人數
+        FollowerCount: user.Followers.length,
+        // 判斷目前登入使用者是否已追蹤該 User 物件
+        isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+      }))
+      // 依追蹤者人數排序清單
+      users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+      return res.render('topUser', { users: users })
+    })
   }
 }
 
