5186a8db4ab403e310e300991a42a1a9c67090e6
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-06 20:26:45 +0800

    Add top10 restaurants

--- controllers/restController.js
+++ controllers/restController.js
@@ -3,6 +3,7 @@ const Restaurant = db.Restaurant
 const Category = db.Category
 const Comment = db.Comment
 const User = db.User
+const Favorite = db.Favorite
 
 const pageLimit = 10
 
@@ -31,7 +32,7 @@ const restController = {
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
         isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id),
-        isLiked: req.user.LikedRestaurants.map(d => d.id).includes(r.id),
+        isLiked: req.user.LikedRestaurants.map(d => d.id).includes(r.id)
       }))
       Category.findAll().then(categories => {
         return res.render('restaurants', {
@@ -94,6 +95,28 @@ const restController = {
       ]
     }).then(restaurant => {
       return res.render('dashboard', { restaurant: restaurant })
+    })
+  },
+  getTopRestaurants: (req, res) => {
+    return Restaurant.findAll({
+      include: [
+        { model: User, as: 'FavoritedUsers' }
+      ]
+    }).then(restaurants => {
+      restaurants = restaurants.map(d => (
+        {
+          ...d.dataValues,
+          description: d.description.substring(0, 50),
+          isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(d.id),
+          FavoriteCount: d.FavoritedUsers.length
+        }
+      ))
+      restaurants = restaurants.sort((a, b) => a.FavoriteCount < b.FavoriteCount ? 1 : -1).slice(0, 10)
+
+      return res.render('topRestaurants', {
+        restaurants: restaurants,
+        isAuthenticated: req.isAuthenticated
+      })
     })
   }
 }
