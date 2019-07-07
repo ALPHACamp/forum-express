7ce27f0fb03ecaf657d4be6d5b7a310d50d19f67
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-08 03:25:50 +0800

    refactor adminController.getRestaurants

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,11 +6,12 @@ const Restaurant = db.Restaurant
 const User = db.User
 const Category = db.Category
 
+const adminService = require('../services/adminService.js')
+
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ include: [Category] }).then(restaurants => {
-      console.log(restaurants)
-      return res.render('admin/restaurants', { restaurants: restaurants })
+    adminService.getRestaurants(req, res, (data) => {
+      return res.render('admin/restaurants', data)
     })
   },
   createRestaurant: (req, res) => {
