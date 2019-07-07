93884e23b219d012e31e18dab7154baa712b6615
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-08 04:05:36 +0800

    DELETE /api/admin/restaurants/:id

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -15,7 +15,14 @@ const adminController = {
       return res.json(data)
     })
   },
+  deleteRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id)
+      .then((restaurant) => {
+        restaurant.destroy()
+          .then((restaurant) => {
+            return res.json({ status: 'success', message: '' })
+          })
+      })
+  }
 }
 module.exports = adminController
-
-
