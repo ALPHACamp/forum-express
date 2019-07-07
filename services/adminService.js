2e3eb2d42c943da9cea0be8cec9de64f0f48946d
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-08 04:25:22 +0800

    refactor adminController.deleteRestaurant

--- services/adminService.js
+++ services/adminService.js
@@ -13,6 +13,15 @@ const adminService = {
       callback({ restaurant: restaurant})
     })
   },
+  deleteRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id)
+      .then((restaurant) => {
+        restaurant.destroy()
+          .then((restaurant) => {
+            callback({ status: 'success', message: '' })
+          })
+      })
+  }
 }
 
 module.exports = adminService
