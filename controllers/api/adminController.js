75609c1f29e871fdac3379dc2157ceeb27a1e3a5
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-08 08:03:38 +0800

    POST /api/admin/restaurants

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -10,6 +10,11 @@ const adminController = {
       return res.json(data)
     })
   },
+  postRestaurant: (req, res) => {
+    adminService.postRestaurant(req, res, (data) => {
+      return res.json(data)
+    })
+  },
   getRestaurant: (req, res) => {
     adminService.getRestaurant(req, res, (data) => {
       return res.json(data)
--- routes/apis.js
+++ routes/apis.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
+router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
 router.get('/admin/categories', categoryController.getCategories)
