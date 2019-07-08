daadd5457e6f2fc4f6558cec026b79b7f857f820
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-08 08:25:13 +0800

    DELETE /api/admin/categories/:id

--- routes/apis.js
+++ routes/apis.js
@@ -16,5 +16,6 @@ router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/categories', categoryController.postCategory)
 router.put('/admin/categories/:id', categoryController.putCategory)
+router.delete('/admin/categories/:id', categoryController.deleteCategory)
 
 module.exports = router
--- services/categoryService.js
+++ services/categoryService.js
@@ -40,5 +40,14 @@ let categoryService = {
         })
     }
   },
+  deleteCategory: (req, res, callback) => {
+    return Category.findByPk(req.params.id)
+      .then((category) => {
+        category.destroy()
+          .then((category) => {
+            callback({ status: 'success', message: ''})
+          })
+      })
+  }
 }
 module.exports = categoryService
