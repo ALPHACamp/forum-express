14ae5c9a44dc69776958cd8a6829da5e663c9e32
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-18 15:29:38 +0800

    POST /api/signin (return jwt)

--- routes/apis.js
+++ routes/apis.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
+const userController = require('../controllers/api/userController.js')
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
 
@@ -17,5 +18,8 @@ router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/categories', categoryController.postCategory)
 router.put('/admin/categories/:id', categoryController.putCategory)
 router.delete('/admin/categories/:id', categoryController.deleteCategory)
+
+// JWT signin
+router.post('/signin', userController.signIn)
 
 module.exports = router
