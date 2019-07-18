0334eb1ad4dff65b8fb79b58c8a5b309216a9df5
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-19 04:23:58 +0800

    userService & api/userController

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../../models')
 const User = db.User
+const userService = require('../../services/userService.js')
 
 // JWT
 const jwt = require('jsonwebtoken')
@@ -55,6 +56,53 @@ let userController = {
         }
       })
     }
+  },
+  getUser: (req, res) => {
+    userService.getUser(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  putUser: (req, res) => {
+    userService.putUser(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  addFavorite: (req, res) => {
+    userService.addFavorite(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+
+  removeFavorite: (req, res) => {
+    userService.removeFavorite(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  addLike: (req, res) => {
+    userService.addLike(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+
+  removeLike: (req, res) => {
+    userService.removeLike(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  getTopUser: (req, res) => {
+    userService.getTopUser(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  addFollowing: (req, res) => {
+    userService.addFollowing(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  removeFollowing: (req, res) => {
+    userService.removeFollowing(req, res, (data) => {
+      return res.json(data)
+    })
   }
 }
 
