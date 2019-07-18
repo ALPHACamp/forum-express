df698917ddd7e0c615af12f5f31afd3f92b70215
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-19 03:29:07 +0800

    restService & api/restController

--- controllers/api/restController.js
+++ controllers/api/restController.js
@@ -1,29 +1,29 @@
-const restService = require('../services/restService.js')
+const restService = require('../../services/restService.js')
 
 const restController = {
   getRestaurants: (req, res) => {
     restService.getRestaurants(req, res, (data) => {
-      return res.render('restaurants', data)
+      return res.json(data)
     })
   },
   getRestaurant: (req, res) => {
     restService.getRestaurant(req, res, (data) => {
-      return res.render('restaurant', data)
+      return res.json(data)
     })
   },
   getFeeds: (req, res) => {
     restService.getFeeds(req, res, (data) => {
-      return res.render('feeds', data)
+      return res.json(data)
     })
   },
   getDashboard: (req, res) => {
     restService.getDashboard(req, res, (data) => {
-      return res.render('dashboard', data)
+      return res.json(data)
     })
   },
   getTopRestaurants: (req, res) => {
     restService.getTopRestaurants(req, res, (data) => {
-      return res.render('topRestaurants', data)
+      return res.json(data)
     })
   }
 }
---
+++ services/restService.js
@@ -0,0 +1,124 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+const Comment = db.Comment
+const User = db.User
+const Favorite = db.Favorite
+
+const pageLimit = 10
+
+const restService = {
+  getRestaurants: (req, res, callback) => {
+    let offset = 0
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['categoryId'] = categoryId
+    }
+    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit }).then(result => {
+      // data for pagination
+      let page = Number(req.query.page) || 1
+      let pages = Math.ceil(result.count / pageLimit)
+      let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = page - 1 < 1 ? 1 : page - 1
+      let next = page + 1 > pages ? pages : page + 1
+
+      // clean up restaurant data
+      const data = result.rows.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50),
+        isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id),
+        isLiked: req.user.LikedRestaurants.map(d => d.id).includes(r.id)
+      }))
+      Category.findAll().then(categories => {
+        return callback({
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
+        })
+      })
+    })
+  },
+  getRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: User, as: 'FavoritedUsers' },
+        { model: User, as: 'LikedUsers' },
+        { model: Comment, include: [User] }
+      ]
+    }).then(restaurant => {
+      restaurant.viewCounts += 1
+      restaurant.save()
+        .then(restaurant => {
+          const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
+          const isLiked = restaurant.LikedUsers.map(d => d.id).includes(req.user.id)
+          callback({
+            restaurant: restaurant,
+            isFavorited: isFavorited,
+            isLiked: isLiked
+          })
+        })
+    })
+  },
+  getFeeds: (req, res, callback) => {
+    return Restaurant.findAll({
+      limit: 10,
+      order: [['createdAt', 'DESC']],
+      include: [Category]
+    }).then(restaurants => {
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant]
+      }).then(comments => {
+        callback({
+          restaurants: restaurants,
+          comments: comments
+        })
+      })
+    })
+  },
+  getDashboard: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    }).then(restaurant => {
+      return callback({ restaurant: restaurant })
+    })
+  },
+  getTopRestaurants: (req, res, callback) => {
+    return Restaurant.findAll({
+      include: [
+        { model: User, as: 'FavoritedUsers' }
+      ]
+    }).then(restaurants => {
+      restaurants = restaurants.map(d => (
+        {
+          ...d.dataValues,
+          description: d.description.substring(0, 50),
+          isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(d.id),
+          FavoriteCount: d.FavoritedUsers.length
+        }
+      ))
+      restaurants = restaurants.sort((a, b) => a.FavoriteCount < b.FavoriteCount ? 1 : -1).slice(0, 10)
+
+      return callback({
+        restaurants: restaurants,
+        isAuthenticated: req.isAuthenticated
+      })
+    })
+  }
+}
+
+module.exports = restService
