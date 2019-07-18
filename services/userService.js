ea4910f18e809a3f57f24d36e34d920d1730f3dd
Ellen Lee <leechinghui.tw@gmail.com>
2019-07-19 04:23:58 +0800

    userService & api/userController

---
+++ services/userService.js
@@ -0,0 +1,150 @@
+const bcrypt = require('bcrypt-nodejs')
+const db = require('../models')
+const User = db.User
+const Restaurant = db.Restaurant
+const Comment = db.Comment
+const Favorite = db.Favorite
+const Like = db.Like
+const Followship = db.Followship
+
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
+const userService = {
+  getUser: (req, res, callback) => {
+    return User.findByPk(req.params.id, {
+      include: [
+        { model: Comment, include: Restaurant },
+        { model: Restaurant, as: 'FavoritedRestaurants' },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
+    }).then(user => {
+      user.CommentsRestaurants = []
+      user.Comments.map((d) => {
+        if (!user.CommentsRestaurants.map(d => d.id).includes(d.RestaurantId)) {
+          user.CommentsRestaurants.push(d.Restaurant)
+          return d
+        }
+      })
+      const isFollowed = req.user.Followings.map(d => d.id).includes(user.id)
+      callback({ profile: user, isFollowed: isFollowed })
+    })
+  },
+  putUser: (req, res, callback) => {
+    if (Number(req.params.id) !== Number(req.user.id)) {
+      return callback({ status: 'error', message: 'permission denied' })
+    }
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              image: img.data.link
+            }).then((user) => {
+              return callback({ status: 'success', message: 'updated successfully' })
+            })
+          })
+      })
+    } else {
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name
+          }).then((user) => {
+            return callback({ status: 'success', message: 'updated successfully' })
+          })
+        })
+    }
+  },
+  addFavorite: (req, res, callback) => {
+    return Favorite.create({
+      UserId: req.user.id,
+      RestaurantId: req.params.restaurantId
+    })
+      .then((restaurant) => {
+        return callback({ status: 'success', message: '' })
+      })
+  },
+
+  removeFavorite: (req, res, callback) => {
+    return Favorite.findOne({
+      where: {
+        UserId: req.user.id,
+        RestaurantId: req.params.restaurantId
+      }
+    })
+      .then((favorite) => {
+        favorite.destroy()
+          .then((restaurant) => {
+            return callback({ status: 'success', message: '' })
+          })
+      })
+  },
+  addLike: (req, res, callback) => {
+    return Like.create({
+      UserId: req.user.id,
+      RestaurantId: req.params.restaurantId
+    }).then((restaurant) => {
+      return callback({ status: 'success', message: '' })
+    })
+  },
+
+  removeLike: (req, res, callback) => {
+    return Like.findOne({
+      where: {
+        UserId: req.user.id,
+        RestaurantId: req.params.restaurantId
+      }
+    }).then((like) => {
+      like.destroy()
+        .then((restaurant) => {
+          return callback({ status: 'success', message: '' })
+        })
+    })
+  },
+  getTopUser: (req, res, callback) => {
+    return User.findAll({
+      include: [
+        { model: User, as: 'Followers' }
+      ]
+    }).then(users => {
+      users = users.map(user => ({
+        ...user.dataValues,
+        FollowerCount: user.Followers.length,
+        isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+      }))
+      users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+      callback({ users: users })
+    })
+  },
+  addFollowing: (req, res, callback) => {
+    return Followship.create({
+      followerId: req.user.id,
+      followingId: req.params.userId
+    })
+      .then((followship) => {
+        return callback({ status: 'success', message: '' })
+      })
+  },
+
+  removeFollowing: (req, res, callback) => {
+    return Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId
+      }
+    })
+      .then((followship) => {
+        followship.destroy()
+          .then((followship) => {
+            return callback({ status: 'success', message: '' })
+          })
+      })
+  }
+}
+
+module.exports = userService
