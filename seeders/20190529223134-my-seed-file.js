8fd332e958d331d4f5d691ce69d54b6e4d617d47
Ellen Lee <leechinghui.tw@gmail.com>
2020-02-28 22:36:29 +0800

    update seeder

--- seeders/20190529223134-my-seed-file.js
+++ seeders/20190529223134-my-seed-file.js
@@ -6,7 +6,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     // generate user seed data
     queryInterface.bulkInsert('Users', [{
-      // id: 1,
+      id: 1,
       email: 'root@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
@@ -14,7 +14,7 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      // id: 2,
+      id: 2,
       email: 'user1@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
@@ -22,7 +22,7 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      // id: 3,
+      id: 3,
       email: 'user2@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
@@ -36,7 +36,7 @@ module.exports = {
       ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
         .map((item, index) =>
           ({
-            // id: index + 1,
+            id: index + 1,
             name: item,
             createdAt: new Date(),
             updatedAt: new Date()
@@ -47,7 +47,7 @@ module.exports = {
     queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map((d, i) =>
         ({
-          // id: i + 1,
+          id: i + 1,
           name: faker.name.findName(),
           tel: faker.phone.phoneNumber(),
           address: faker.address.streetAddress(),
@@ -65,7 +65,7 @@ module.exports = {
     return queryInterface.bulkInsert('Comments',
       [...Array(150)].map((item, index) => index).map(i =>
         ({
-          // id: i + 1,
+          id: i + 1,
           text: faker.lorem.sentence(),
           UserId: Math.floor(Math.random() * 3) + 1,
           RestaurantId: i % 50 + 1,
